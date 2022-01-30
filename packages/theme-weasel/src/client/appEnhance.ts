1d683fbe608cea2677c1b5d5734b17494f6e29a0
CavinHuang <sujinw@qq.com>
2022-01-30 16:40:07 +0800

    layout entry

--- packages/theme-weasel/src/client/appEnhance.ts
+++ packages/theme-weasel/src/client/appEnhance.ts
@@ -3,6 +3,6 @@ import { defineClientAppEnhance } from "@vuepress/client";
 // import style
 import "./styles/index.scss";
 
-// clinet rukou 
+// clinet entry
 export default defineClientAppEnhance(() => {
 });
