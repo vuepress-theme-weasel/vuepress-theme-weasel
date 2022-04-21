87b9328cda65048171d62c35b0356d0abbf3fa26
cavinHuang <943866180@qq.com>
2022-04-21 22:49:26 +0800

    feat: (theme) add blog plugin for theme

---
+++ packages/theme/src/node/plugins/blog.ts
@@ -0,0 +1,17 @@
+/**
+ * blog
+ */
+
+const defaultOptions: ThemeBlogPluginOptions = {
+  article: '/article/',
+  category: '/category/',
+  categoryItem: '/category/:name/',
+  tag: '/tag/',
+  tagItem: '/tag/:name/',
+  encrypted: '/encrypted/',
+  slides: '/slides/',
+  star: '/star/',
+  timeline: '/timeline/',
+}
+
+export const resolveBlog = () => {}
