099d927a508008e3266d51b2d29811d71dc2f17f
CavinHuang <sujinw@qq.com>
2022-05-11 17:23:53 +0800

    chore(release): publish v1.0.0-beta.1

--- demo/src/.vuepress/config.ts
+++ demo/src/.vuepress/config.ts
@@ -35,53 +35,53 @@ export default defineUserConfig({
   theme: '@mr-huang/vuepress-theme-weasel',
   themeConfig,
   plugins: [
-    ['@vuepress/plugin-docsearch', {
-      appId: "LDBQGQC8Q9",
-      apiKey: "5c3a7145aeba231c3b85b742d24fc24f",
-      indexName: "mrhope",
-      locales: {
-        "/": {
-          placeholder: "搜索",
-          translations: {
-            button: {
-              buttonText: "搜索",
-              buttonAriaLabel: "搜索",
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: "清除查询条件",
-                resetButtonAriaLabel: "清除查询条件",
-                cancelButtonText: "取消",
-                cancelButtonAriaLabel: "取消",
-              },
-              startScreen: {
-                recentSearchesTitle: "搜索历史",
-                noRecentSearchesText: "没有搜索历史",
-                saveRecentSearchButtonTitle: "保存至搜索历史",
-                removeRecentSearchButtonTitle: "从搜索历史中移除",
-                favoriteSearchesTitle: "收藏",
-                removeFavoriteSearchButtonTitle: "从收藏中移除",
-              },
-              errorScreen: {
-                titleText: "无法获取结果",
-                helpText: "你可能需要检查你的网络连接",
-              },
-              footer: {
-                selectText: "选择",
-                navigateText: "切换",
-                closeText: "关闭",
-                searchByText: "搜索提供者",
-              },
-              noResultsScreen: {
-                noResultsText: "无法找到相关结果",
-                suggestedQueryText: "你可以尝试查询",
-                reportMissingResultsText: "你认为该查询应该有结果？",
-                reportMissingResultsLinkText: "点击反馈",
-              },
-            },
-          },
-        },
-      }
-    }]
+    // ['@vuepress/plugin-docsearch', {
+    //   appId: "LDBQGQC8Q9",
+    //   apiKey: "5c3a7145aeba231c3b85b742d24fc24f",
+    //   indexName: "mrhope",
+    //   locales: {
+    //     "/": {
+    //       placeholder: "搜索",
+    //       translations: {
+    //         button: {
+    //           buttonText: "搜索",
+    //           buttonAriaLabel: "搜索",
+    //         },
+    //         modal: {
+    //           searchBox: {
+    //             resetButtonTitle: "清除查询条件",
+    //             resetButtonAriaLabel: "清除查询条件",
+    //             cancelButtonText: "取消",
+    //             cancelButtonAriaLabel: "取消",
+    //           },
+    //           startScreen: {
+    //             recentSearchesTitle: "搜索历史",
+    //             noRecentSearchesText: "没有搜索历史",
+    //             saveRecentSearchButtonTitle: "保存至搜索历史",
+    //             removeRecentSearchButtonTitle: "从搜索历史中移除",
+    //             favoriteSearchesTitle: "收藏",
+    //             removeFavoriteSearchButtonTitle: "从收藏中移除",
+    //           },
+    //           errorScreen: {
+    //             titleText: "无法获取结果",
+    //             helpText: "你可能需要检查你的网络连接",
+    //           },
+    //           footer: {
+    //             selectText: "选择",
+    //             navigateText: "切换",
+    //             closeText: "关闭",
+    //             searchByText: "搜索提供者",
+    //           },
+    //           noResultsScreen: {
+    //             noResultsText: "无法找到相关结果",
+    //             suggestedQueryText: "你可以尝试查询",
+    //             reportMissingResultsText: "你认为该查询应该有结果？",
+    //             reportMissingResultsLinkText: "点击反馈",
+    //           },
+    //         },
+    //       },
+    //     },
+    //   }
+    // }]
   ]
 })
