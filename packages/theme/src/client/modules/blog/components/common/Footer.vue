f65baaaa5fd310569978b7bb9ce8fd9c49faa0dd
CavinHuang <sujinw@qq.com>
2022-10-12 10:49:44 +0800

    feat: 增加备案信息

--- packages/theme/src/client/modules/blog/components/common/Footer.vue
+++ packages/theme/src/client/modules/blog/components/common/Footer.vue
@@ -12,6 +12,12 @@
     <div class="footer">
       <p v-if="footer">{{ footer }}</p>
       <a href="https://beian.miit.gov.cn">粤ICP备2022100010号-1</a>
+      <div style="width:300px;margin:0 auto; padding:20px 0;">
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44030602007137"
+          style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="https://www.beian.gov.cn/img/ghs.png" style="float:left;" />
+          <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">粤公网安备 44030602007137号</p>
+        </a>
+    </div>
     </div>
   </footer>
 </template>
